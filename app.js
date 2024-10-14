7bd8a2308f36939f77f19a07e4332a1c86f081d0
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-14 09:56:52 -0300

    total de paginas de usuarios

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const apiUrl = "https://67084a4f8e86a8d9e42e96a5.mockapi.io/api/users";
 let usuarios= [];
+const usuariosPorPagina = 9;
+const paginaAtual = 1;
+
+
+
 
  async function fetchUsuarios()
 {
@@ -18,11 +23,38 @@ let usuarios= [];
     try {   
         const response = await axios.get(apiUrl);
         usuarios = response.data;
-        displayUsers(usuarios)
+        displayPaginado()
     } catch (error) {
         Swal.fire('erro' , 'nao foi possivel buascar usuario' , "error")
     }
 }
+function displayPaginado(page = 1)
+{
+    const inicio = (page - 1) * usuariosPorPagina;
+    const fim = page * usuariosPorPagina;
+    const usuariosPaginados = usuarios.slice(inicio , fim);
+    displayUsers(usuariosPaginados);
+    configurarPaginacao(usuarios.length, page);
+};
+
+    function configurarPaginacao(totalusuarios, page){
+        const totalPages = Math.ceil(totalusuarios / usuariosPorPagina);
+        const pagination = document.getElementById('paginacao')
+        pagination.innerHTML = '';
+
+        for(let i = 1; i <= totalPages; i++){
+            const li = document.createElement('li');
+            li.className = `page-item ${i === page ? "active" : ""}`;
+            li.innerHTML = `<button class="page-link" onclick="displayPaginado(${i})">${i}</button>`
+            pagination.appendChild(li);
+        }
+        }
+
+
+
+
+
+
 fetchUsuarios()
 
 function displayUsers(usuarios)
@@ -31,7 +63,7 @@ function displayUsers(usuarios)
     userList.innerHTML = ''
     usuarios.forEach(user => {
         const usuario = document.createElement('div')
-        usuario.className = 'usar-card'; 
+        usuario.className = 'col-md-4 mb-3'; 
         usuario.innerHTML = `
             <img src="${user.avatar}" alt="${user.firstName}" class="img rounded-circle" style="max-width: 150px;">
 
