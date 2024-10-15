0b6d963167b3ac6eb9335790b06b9619becb1a92
daviiisousa <sousadavi248@gmail.com>
2024-10-15 15:49:43 -0300

    funçao de delete

--- app.js
+++ app.js
@@ -3,109 +3,108 @@ let usuarios = [];
 const usuariosPorPagina = 9;
 const paginaAtual = 1;
 
-const modal = document.getElementById('userModal')
+const modal = document.getElementById("userModal");
 
 async function fetchUsuarios() {
-    // fetch(apiUrl)
-    // .then(res => res.json())
-    // .then(data => {
-    //     usuarios = data;
-    //     displayUsers(usuarios)
-    // })
-    // .catch(error => {
-    //     console.error("Erro", error)
-    //     alert("Não foi possível carregar o usuário!")
-    // })
 
-    try {
-        const response = await axios.get(apiUrl);
-        usuarios = response.data;
-        displayPaginado();
-    } catch (error) {
-        Swal.fire("Erro", "Não foi possível buscar usuários", "error")
-    }
-    
+  try {
+    const response = await axios.get(apiUrl);
+    usuarios = response.data;
+    displayPaginado();
+  } catch (error) {
+    Swal.fire("Erro", "Não foi possível buscar usuários", "error");
+  }
 }
 
-function displayPaginado(page = 1){
-    const inicio = (page - 1) * usuariosPorPagina;
-    const fim = page * usuariosPorPagina;
-    const usuariosPaginados = usuarios.slice(inicio, fim);
-    displayUsers(usuariosPaginados);
-    configurarPaginacao(usuarios.length, page);
+function displayPaginado(page = 1) {
+  const inicio = (page - 1) * usuariosPorPagina;
+  const fim = page * usuariosPorPagina;
+  const usuariosPaginados = usuarios.slice(inicio, fim);
+  displayUsers(usuariosPaginados);
+  configurarPaginacao(usuarios.length, page);
 }
 
 function configurarPaginacao(totalusuarios, page) {
-    const totalPages = Math.ceil(totalusuarios / usuariosPorPagina);
-    const pagination = document.getElementById("paginacao")
-    pagination.innerHTML = "";
+  const totalPages = Math.ceil(totalusuarios / usuariosPorPagina);
+  const pagination = document.getElementById("paginacao");
+  pagination.innerHTML = "";
 
-    for (let i = 1; i <= totalPages; i++) {
-        const li = document.createElement("li");
-        li.className = `page-item ${i === page ? "active" : ""}`;
-        li.innerHTML = `<button class="page-link" onclick="displayPaginado(${i})">${i}</button>`;
-        pagination.appendChild(li);
-    }
+  for (let i = 1; i <= totalPages; i++) {
+    const li = document.createElement("li");
+    li.className = `page-item ${i === page ? "active" : ""}`;
+    li.innerHTML = `<button class="page-link" onclick="displayPaginado(${i})">${i}</button>`;
+    pagination.appendChild(li);
+  }
 }
 
 function displayUsers(usuarios) {
-    const userList = document.getElementById("listaDeUsuarios");
-    userList.innerHTML = "";
-    usuarios.forEach(user => {
-        const usuario = document.createElement("div");
-        usuario.className = 'col-md-4 mb-3';
-        usuario.innerHTML = `
-            <img src="${user.avatar}" alt="${user.firstName}" class="img rounded-circle" style="max-width: 150px;">
-            <h3>${user.firstName} ${user.lastName}</h3>
-            <p>Email: ${user.email}</p>
-            <p>Telefone: ${user.phone}</p>
-        `
-        userList.appendChild(usuario);
-    })
+  const userList = document.getElementById("listaDeUsuarios");
+  userList.innerHTML = "";
+  usuarios.forEach((user) => {
+    const usuario = document.createElement("div");
+    usuario.className = "col-md-4 mb-3";
+    usuario.innerHTML = `
+            <div><img src="${user.avatar}" alt="${user.firstName}" class="img rounded-circle" style="max-width: 150px;"></div>
+            <div>
+                <h3>${user.firstName} ${user.lastName}</h3>
+                <p>Email: ${user.email}</p>
+                <p>Telefone: ${user.phone}</p>
+                <box-icon onclick="deleteUsuario(${user.id})" name='trash-alt'></box-icon>
+            </div>
+        `;
+
+    userList.appendChild(usuario);
+  });
 }
 
-function showCreateModal(){
-    modal.style.display = 'block'
-    modal.className = 'modal'
+function showCreateModal() {
+  modal.style.display = "block";
+  modal.className = "modal";
+  buttonEdit.style.display = 'none'
 }
 
-function fechar(){
-    modal.style.display = 'none'
+function fechar() {
+  modal.style.display = "none";
 }
 
-async function postUsuario(){
+async function postUsuario() {
+  const firstName = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const avatar = document.getElementById("avatar").value;
+  const lastName = document.getElementById("Sobrenome").value;
 
-    const firstName = document.getElementById('name').value  
-    const email = document.getElementById('email').value
-    const phone = document.getElementById("phone").value
-    const avatar = document.getElementById('avatar').value
-    const lastName = document.getElementById('Sobrenome').value
+  const payload = {
+    firstName,
+    lastName,
+    email,
+    phone,
+    avatar,
+  };
 
-    const payload = {
-        firstName,
-        lastName,
-        email,
-        phone,
-        avatar
-    }
+  console.log(payload);
 
-    console.log(payload);
-    
+  const method = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
 
-    const method ={
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-    }
-
-    await fetch(apiUrl, method)
+  await fetch(apiUrl, method);
+  window.location.reload()
 }
 
-function saveUser(){
-    postUsuario()
-    modal.style.display = 'none'
+function saveUser() {
+  postUsuario();
+
+  modal.style.display = "none";
 }
 
-document.addEventListener("DOMContentLoaded", fetchUsuarios)
+async function deleteUsuario(id){
+    await axios.delete(`${apiUrl}/${id}`)
+    window.location.reload()
+}
+
+document.addEventListener("DOMContentLoaded", fetchUsuarios);
